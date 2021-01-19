4b78dba114c113c120d9c1f304a1239276be454d
Daniel <daniel@sdunixgeek.com>
2021-01-19 06:19:57 -0800

    Support netmask for VPN CIDR allocation
    * Allow defining a SRVIPNETMASK that will be used in all applicable configurations including the previously omitted `ifconfig tap_soft` in `build/services.d/softether-vpnsrv/run` script. This addresses issue #9
    * Updated 10-routing.sh to set the CIDR subnet using a function to convert the supplied `xx.xx.xx.xx` netmask.
    * Updated 20-dhcp-conf.sh  to additionally replace SRVIPNETMASK defaulting to `255.255.255.0` which was the original default hard coded value.
    * Updated README.md and init-env.sh to specify the added SRVIPNETMASK value.

--- build/cont-init.d/10-routing.sh
+++ build/cont-init.d/10-routing.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # Create IP tables rules
+function convMask () {
+  c=0 x=0$( printf '%o' ${1//./ } )
+  while [ $x -gt 0 ]; do
+      let c+=$((x%2)) 'x>>=1'
+  done
+  echo "$c";
+}
 echo "Creating postrouting rules."
-iptables -t nat -A POSTROUTING -s ${SRVIPSUBNET:-10.0.0}.0/24 -j MASQUERADE
+cidrNet=$(convMask ${SRVIPNETMASK:-255.255.255.0})
+iptables -t nat -A POSTROUTING -s ${SRVIPSUBNET:-10.0.0}.0/${cidrNet:-24} -j MASQUERADE
--- build/cont-init.d/20-dhcp-conf.sh
+++ build/cont-init.d/20-dhcp-conf.sh
@@ -6,4 +6,4 @@ if [ ! -f /cfg/dnsmasq.conf ]; then
   cp /etc/dnsmasq.conf.default /cfg/dnsmasq.conf
 fi
 
-sed "s/\$SRVIPSUBNET/${SRVIPSUBNET:-10.0.0}/g" /cfg/dnsmasq.conf >/etc/dnsmasq.conf
+sed -e "s/\$SRVIPSUBNET/${SRVIPSUBNET:-10.0.0}/g" -e "s/\$SRVIPNETMASK/${SRVIPNETMASK:-255.255.255.0}/g" /cfg/dnsmasq.conf >/etc/dnsmasq.conf
